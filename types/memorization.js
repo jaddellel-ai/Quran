c017fb00be5c00ec1d17c0441c9ac62365477094
jaddellel-ai <jaddellel@gmail.com>
2025-09-14 23:09:08 +0200

    Add files via upload

---
+++ types/memorization.js
@@ -0,0 +1,16 @@
+export const MemorizationStatus = {
+  NOT_STARTED: 'not_started',
+  LEARNING: 'learning',
+  REVIEWING: 'reviewing',
+  MASTERED: 'mastered'
+};
+
+export const RepetitionInterval = {
+  FIRST: '10m',    // 10 minutes
+  SECOND: '1d',    // 1 day
+  THIRD: '3d',     // 3 days
+  FOURTH: '1w',    // 1 week
+  FIFTH: '2w',     // 2 weeks
+  SIXTH: '1m',     // 1 month
+  MASTERED: 'mastered'
+};
